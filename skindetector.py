7019d2f86c24719e0e0b2fe9f8efa19c35544742
Raul Garcia <rauld.garcia95@gmail.com>
2022-10-16 18:22:54 -0500

    codigo casi terminado

--- skindetector.py
+++ skindetector.py
@@ -8,9 +8,42 @@ import random
 import math
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 from mpl_toolkits.mplot3d import Axes3D
 import time
+from scipy.special import expit
 inicio=time.time()
+skinc=np.array([[0,0]])
+backc=np.array([[0,0]])
+puntos=10
+clicks=0
+p=math.pi
+lskin=0.6
+lback=0.4
+
+#funcion para obtener los arrays de piel y fondo
+def mouse1(event,x,y,flags,param):
+    global clicks
+    #array para obtener el array de piel
+    if event == cv2.EVENT_LBUTTONDOWN and clicks<puntos:
+        global skinc
+        aux=np.array([[x,y]])
+        skinc=np.append(skinc,aux,axis=0)
+        clicks=clicks+1
+        print('De click en un pixel de piel, faltan:',(puntos-clicks))
+
+    #array para obtener el array de fondo
+    if event == cv2.EVENT_LBUTTONDOWN and clicks<2*puntos+1 and clicks>=puntos:
+        global backc
+        aux=np.array([[x,y]])
+        backc=np.append(backc,aux,axis=0)
+        clicks=clicks+1
+        print('De click en un pixel de fondo, faltan:',(2*puntos-clicks+1))
+
+    #funcion para cerrar clickeo en imagen
+    if clicks==2*puntos+1:
+        cv2.destroyAllWindows()
+    
 
 #Transpuesta de matriz
 def transpose(matrix):
@@ -24,17 +57,24 @@ def transpose(matrix):
     return result
 
 #Carga de imagen
-imagen = cv2.imread('imagen4.jpg')
+imagen = cv2.imread('imagen1.jpg')
 #cv2.imshow('imagen',imagen)
-cv2.waitKey(0)
-resolucionx=1366
-resoluciony=772
+resolucionx=800
+resoluciony=594
 print("La resolución de la imagen es:")
-print(resolucionx)
-print(resoluciony)
+print('X:',resolucionx)
+print('Y:',resoluciony)
 print("\nEl total de pixeles es:")
 print(resolucionx*resoluciony)
+print('De click en un pixel de piel faltan:',(puntos-clicks))
 
+while clicks<2*puntos+1:
+    cv2.waitKey(0) & 0xFF
+    cv2.namedWindow('imagen')
+    cv2.setMouseCallback('imagen',mouse1)
+    cv2.imshow('imagen',imagen)
+    
+#---------------------------------------------------------CALCULO DE VECTOR MEDIO PIEL--------------------------------------------------------   
 #Vector auxiliar para calcular vector promedio
 sumrgb=np.zeros((3,1))
 
@@ -42,163 +82,221 @@ sumrgb=np.zeros((3,1))
 cont=0
 
 #Creacion de vector promedio
-vm=np.zeros((3,1))
-
-#Creación de arrays para ploteo de colores
-x1=np.zeros((1,1))
-y1=np.zeros((1,1))
-z1=np.zeros((1,1))
-color=np.zeros((1,3))
+vmskin=np.zeros((3,1))
 
 #Extraer componentes B,G,R de un solo pixel (opencv los da en ese orden y se cargan las coordenadas en orden Y X)
-for y in range (resoluciony):
-    for x in range (resolucionx):
-        (b, g, r) = imagen[y, x]
-        rgb=np.array([
-            [r],
-            [g],
-            [b]
-        ])
-        crgb=np.array([[r/255,g/255,b/255]]) #array que guarda color de pixel actual
-        rgb=rgb/255;
-        color=np.append(color,crgb,axis=0)   #array que guarda color de cada pixel
-        sumrgb=sumrgb+rgb
-        cont=cont+1
-
-#Calculo de vector promedio
-vm=sumrgb/cont;
-print("\nEl vector promedio es:")
-print(vm)
-
-#Creacion de matriz auxiliar
-sumamatriz=np.zeros((3,3))
-cont=0
-
-#Extraer componentes B,G,R de un solo pixel (opencv los da en ese orden y se cargan las coordenadas en orden Y X)
-for y in range (resoluciony):
-    for x in range (resolucionx):
-        (b, g, r) = imagen[y, x]
-        rgb=np.array([
-            [r],
-            [g],
-            [b]
-        ])
-        rgb=rgb/255;
-        
-        #Calculo de vector x-m
-        x_m=rgb-vm
-        #Transpuesta de x-m
-        x_mt=transpose(x_m)
-        #Creacion de matriz (x-m)(x-m)^t
-        matriz=x_m*x_mt
-        sumamatriz=sumamatriz+matriz
-        cont=cont+1
-
-cx=np.zeros((3,3))
-cx=sumamatriz/(cont-1);
-print("\nLa matriz de covarianza es:")
-print(cx)
-
-df=pd.DataFrame(cx)
-cor=df.corr()
-print('\nLa matriz de correlación es:')
-print(cor)
-
-#Calculo del determinante de la matriz de covarianza
-det=np.linalg.det(cx)
-
-#Extraccion de 3 pixeles al azar
-for i in range (1):
-    pixx=random.randint(0,resolucionx)
-    pixy=random.randint(0,resoluciony)
-    (b, g, r) = imagen[pixy,pixx]
+for i in range (1,puntos+1,1):
+    x=skinc[i][0]
+    y=skinc[i][1]
+    (b, g, r) = imagen[y, x]
     rgb=np.array([
         [r],
         [g],
         [b]
     ])
     rgb=rgb/255;
-    print("\nEl pixel es: ({},{})".format(pixx,pixy))
-    print(rgb)
-    x_mu=rgb-vm
-    x_mut=transpose(x_mu)
-    auxiliar2=np.zeros((1,3))
-    cxi=np.linalg.inv(cx)
-    auxiliar1=np.matmul(x_mut,cxi)
-    auxiliar2=np.matmul(auxiliar1,x_mu)
-    n=(1/((2*3.1416)**1.5))*(1/((det)**0.5))*(math.exp(-0.5*auxiliar2))
-    print("La cercania al color promedio es:")
-    print(n)
+    sumrgb=sumrgb+rgb
+    cont=cont+1
 
-#calculo de eigenvalores y eigenvectores 
-eigenvalor, eigenvector=np.linalg.eig(cx)
-print("\nLos eigenvalores son:")
-print(eigenvalor)
-print("\nLos eigenvectores son:")
-print(eigenvector)
+#Calculo de vector promedio
+vmskin=sumrgb/cont;
+print("\nEl vector promedio de piel es:")
+print(vmskin)
 
-den=eigenvalor[0]+eigenvalor[1]+eigenvalor[2]
-pv1=eigenvalor[0]/den
-pv2=eigenvalor[1]/den
-pv3=eigenvalor[2]/den
-print("\nEl porcentaje del total de la varianza para el eigenvalor 1 es:")
-print(pv1*100)
-print("\nEl porcentaje del total de la varianza para el eigenvalor 2 es:")
-print(pv2*100)
-print("\nEl porcentaje del total de la varianza para el eigenvalor 3 es:")
-print(pv3*100)
+#-------------------------------------------CREACION DE MATRIZ DE COVARIANZA DE PIEL-------------------------------------
+#Creacion de matriz auxiliar
+sumamatriz=np.zeros((3,3))
+cont=0
 
-#creacion de vector medio
-vmx=(0,vm[0])
-vmy=(0,vm[1])
-vmz=(0,vm[2])
+#Extraer componentes B,G,R de un solo pixel (opencv los da en ese orden y se cargan las coordenadas en orden Y X)
+for i in range (1,puntos+1,1):
+    x=skinc[i][0]
+    y=skinc[i][1]
+    (b, g, r) = imagen[y, x]
+    rgb=np.array([
+        [r],
+        [g],
+        [b]
+    ])
+    rgb=rgb/255;
+    #Calculo de vector x-m
+    x_m=rgb-vmskin
+    #Transpuesta de x-m
+    x_mt=transpose(x_m)
+    #Creacion de matriz (x-m)(x-m)^t
+    matriz=x_m*x_mt
+    sumamatriz=sumamatriz+matriz
+    cont=cont+1
 
-#creacion de eigenvector 1
-ev1x=(vm[0]+(eigenvector[0][0]))
-ev1y=(vm[1]+(eigenvector[1][0]))
-ev1z=(vm[2]+(eigenvector[2][0]))
-v1x=np.append(vm[0],ev1x)
-v1y=np.append(vm[1],ev1y)
-v1z=np.append(vm[2],ev1z)
+cxskin=np.zeros((3,3))
+cxskin=sumamatriz/(cont-1);
+print("\nLa matriz de covarianza de piel es:")
+print(cxskin)
 
-#creacion de eigenvector 2
-ev2x=(vm[0]+(eigenvector[0][1]))
-ev2y=(vm[1]+(eigenvector[1][1]))
-ev2z=(vm[2]+(eigenvector[2][1]))
-v2x=np.append(vm[0],ev2x)
-v2y=np.append(vm[1],ev2y)
-v2z=np.append(vm[2],ev2z)
+#----------------------------------------------------CALCULO DE VECTOR MEDIO FONDO-------------------------------
+#Vector auxiliar para calcular vector promedio
+sumrgbback=np.zeros((3,1))
 
-#creacion de eigenvector 3
-ev3x=(vm[0]+(eigenvector[0][2]))
-ev3y=(vm[1]+(eigenvector[1][2]))
-ev3z=(vm[2]+(eigenvector[2][2]))
-v3x=np.append(vm[0],ev3x)
-v3y=np.append(vm[1],ev3y)
-v3z=np.append(vm[2],ev3z)
+#Contador auxiliar para calcular promedio
+cont=0
 
-#creacion de los array de xyz
-colores=pd.DataFrame(color)
-x1=colores[0]
-y1=colores[1]
-z1=colores[2]
+#Creacion de vector promedio
+vmback=np.zeros((3,1))
 
-#Grafico
-fig=plt.figure()
-axes=plt.axes(projection='3d')
-axes.scatter3D(x1,y1,z1,c=color)
-axes.set_xlim([0,1])
-axes.set_ylim([0,1])
-axes.set_zlim([0,1])
-axes.plot(vmx,vmy,vmz)
-axes.plot(v1x,v1y,v1z)
-axes.plot(v2x,v2y,v2z)
-axes.plot(v3x,v3y,v3z)
-axes.set_title("Grafico de distribución de pixeles")
-axes.set_xlabel("R")
-axes.set_ylabel("G")
-axes.set_zlabel("B")
-fin=time.time()
-print("El tiempo de ejecución es:")
-print(fin-inicio)
-plt.show()
+#Extraer componentes B,G,R de un solo pixel (opencv los da en ese orden y se cargan las coordenadas en orden Y X)
+for i in range (2,puntos+2,1):
+    x=backc[i][0]
+    y=backc[i][1]
+    (b, g, r) = imagen[y, x]
+    rgb=np.array([
+        [r],
+        [g],
+        [b]
+    ])
+    rgb=rgb/255;
+    sumrgbback=sumrgbback+rgb
+    cont=cont+1
+
+#Calculo de vector promedio
+vmback=sumrgbback/cont;
+print("\nEl vector promedio de fondo es:")
+print(vmback)
+
+#-------------------------------------------CREACION DE MATRIZ DE COVARIANZA DE FONDO-------------------------------------
+#Creacion de matriz auxiliar
+sumamatrizback=np.zeros((3,3))
+cont=0
+
+#Extraer componentes B,G,R de un solo pixel (opencv los da en ese orden y se cargan las coordenadas en orden Y X)
+for i in range (2,puntos+2,1):
+    x=backc[i][0]
+    y=backc[i][1]
+    (b, g, r) = imagen[y, x]
+    rgb=np.array([
+        [r],
+        [g],
+        [b]
+    ])
+    rgb=rgb/255;
+
+    #Calculo de vector x-m
+    x_m=rgb-vmback
+    #Transpuesta de x-m
+    x_mt=transpose(x_m)
+    #Creacion de matriz (x-m)(x-m)^t
+    matriz=x_m*x_mt
+    sumamatrizback=sumamatrizback+matriz
+    cont=cont+1
+    
+
+cxback=np.zeros((3,3))
+cxback=sumamatrizback/(cont-1);
+print("\nLa matriz de covarianza de fondo es:")
+print(cxback)
+
+#--------------------------------PROBABILIDAD PIEL------------------------------
+matriznskin=np.zeros((resoluciony,resolucionx))
+logmatriznskin=np.zeros((resoluciony,resolucionx))
+#Calculo del determinante de la matriz de covarianza
+det=np.linalg.det(cxskin)
+for y in range(resoluciony):
+    for x in range(resolucionx):
+        (b, g, r) = imagen[y, x]
+        rgb=np.array([
+            [r],
+            [g],
+            [b]
+        ])
+        rgb=rgb/255;
+        x_mu=rgb-vmskin
+        x_mut=transpose(x_mu)
+        auxiliar2=np.zeros((1,3))
+        cxi=np.linalg.inv(cxskin)
+        auxiliar1=np.matmul(x_mut,cxi)
+        auxiliar2=np.matmul(auxiliar1,x_mu)
+        matriznskin[y][x]=(1/((2*p)**1.5))*(1/((det)**0.5))*(math.exp(-0.5*auxiliar2))
+        logmatriznskin[y][x]=math.log(matriznskin[y][x],10)
+        
+fig=plt.figure()    
+sns.heatmap(logmatriznskin)
+plt.title('log(p(X|W=piel=1))')
+plt.show
+
+#--------------------------------PROBABILIDAD FONDO------------------------------
+matriznback=np.zeros((resoluciony,resolucionx))
+#Calculo del determinante de la matriz de covarianza
+det=np.linalg.det(cxback)
+for y in range(resoluciony):
+    for x in range(resolucionx):
+        (b, g, r) = imagen[y, x]
+        rgb=np.array([
+            [r],
+            [g],
+            [b]
+        ])
+        rgb=rgb/255;
+        x_mu=rgb-vmback
+        x_mut=transpose(x_mu)
+        auxiliar2=np.zeros((1,3))
+        cxi=np.linalg.inv(cxback)
+        auxiliar1=np.matmul(x_mut,cxi)
+        auxiliar2=np.matmul(auxiliar1,x_mu)
+        prelog=(1/((2*p)**1.5))*(1/((det)**0.5))*(math.exp(-0.5*auxiliar2))
+        matriznback[y][x]=math.log(prelog,10)
+
+fig=plt.figure()    
+sns.heatmap(matriznback)
+plt.title('log(p(X|W=fondo=0))')
+plt.show
+
+
+#----------------------------MAPA DE CALOR DE PIEL-----------------------------
+pskin=expit(matriznskin)
+hm=pskin*lskin;
+fig=plt.figure()    
+sns.heatmap(hm)
+plt.title('MAPA DE CALOR DE PIEL p(W=1|X)')
+plt.show
+
+
+#-------------------------IMAGEN BINARIA 0.4-----------------------------------
+ib=np.zeros((resoluciony,resolucionx))
+for y in range(resoluciony):
+    for x in range(resolucionx):
+        if hm[y][x]>=0.4:
+            ib[y][x]=1
+        else:
+            ib[y][x]=0
+            
+fig=plt.figure()    
+sns.heatmap(ib,vmin=0,vmax=1)
+plt.title('Threshold=0.4')
+plt.show
+
+#-------------------------IMAGEN BINARIA 0.5-----------------------------------
+ib=np.zeros((resoluciony,resolucionx))
+for y in range(resoluciony):
+    for x in range(resolucionx):
+        if hm[y][x]>=0.5:
+            ib[y][x]=1
+        else:
+            ib[y][x]=0
+            
+fig=plt.figure()    
+sns.heatmap(ib,vmin=0,vmax=1)
+plt.title('Threshold=0.5')
+plt.show
+
+#-------------------------IMAGEN BINARIA 0.6-----------------------------------
+ib=np.zeros((resoluciony,resolucionx))
+for y in range(resoluciony):
+    for x in range(resolucionx):
+        if hm[y][x]>=0.6:
+            ib[y][x]=1
+        else:
+            ib[y][x]=0
+            
+fig=plt.figure()    
+sns.heatmap(ib,vmin=0,vmax=1)
+plt.title('Threshold=0.6')
+plt.show
